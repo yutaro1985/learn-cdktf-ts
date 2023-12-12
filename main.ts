1962f31dd981a07e8528e8487c715c8e3a256077
Yutaro Suzuki <yutaro.suzuki@cloudys.dev>
2023-12-12 22:07:03 +0900

    EC2作成時点のコード

--- main.ts
+++ main.ts
@@ -1,14 +1,39 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
+import { DataAwsSsmParameter } from "@cdktf/provider-aws/lib/data-aws-ssm-parameter";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    // define rcdesources here
+    new AwsProvider(this, "aws", {
+      region: "ap-northeast-1",
+      profile: "privateadmin",
+    });
+
+    const amazonLinux2023Latest = new DataAwsSsmParameter(
+      this,
+      "AL2023latest",
+      {
+        name: "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64",
+      }
+    );
+
+    const ec2Instance = new Instance(this, "compute", {
+      ami: amazonLinux2023Latest.value,
+      instanceType: "t2.micro",
+    });
+
+    new TerraformOutput(this, "public_ip", {
+      value: ec2Instance.publicIp,
+    });
   }
 }
 
 const app = new App();
-new MyStack(app, "learn-cdktf-ts");
+new MyStack(app, "aws_instance");
+
 app.synth();
