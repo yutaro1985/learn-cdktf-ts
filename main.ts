db1782f735daff4a9b2ea4a863ef97b8bf41c3e6
Yutaro Suzuki <yutaro.suzuki@cloudys.dev>
2023-12-15 00:17:22 +0900

    VPC+EC2を作成時のソース

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { Construct } from "constructs";
-import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { App, TerraformOutput, TerraformStack, Token, Fn } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
 import { DataAwsSsmParameter } from "@cdktf/provider-aws/lib/data-aws-ssm-parameter";
-import { Vpc } from "./modules/vpc";
+import { Vpc } from "./.gen/modules/vpc";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -18,9 +18,9 @@ class MyStack extends TerraformStack {
     const vpc = new Vpc(this, "vpc", {
       projectName: "advent_calendar",
       env: "dev",
-      vpc_cidr_block: "10.0.0.0/16",
-      az_suffixes: ["a", "c", "d"],
-      create_ssm_endpoint: true,
+      vpcCidrBlock: "10.0.0.0/16",
+      azSuffixes: ["a", "c", "d"],
+      createSsmEndpoint: true,
     });
 
     const amazonLinux2023Latest = new DataAwsSsmParameter(
@@ -30,15 +30,22 @@ class MyStack extends TerraformStack {
         name: "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64",
       }
     );
-
+    const public_subnets = Fn.lookup(
+      Token.asAnyMap(vpc.outputsOutput),
+      "public_subnets"
+    );
     const ec2Instance = new Instance(this, "compute", {
       ami: amazonLinux2023Latest.value,
       instanceType: "t2.micro",
-      subnetId: vpc.publicSubnets[0].id,
+      subnetId: Fn.element(public_subnets, Math.floor(Math.random() * 3)),
     });
 
-    new TerraformOutput(this, "public_ip", {
-      value: ec2Instance.publicIp,
+    let outputs = new Map();
+    outputs.set("public_ip", ec2Instance.publicIp);
+    outputs.set("subnet", ec2Instance.availabilityZone);
+
+    new TerraformOutput(this, "outputs", {
+      value: outputs,
     });
   }
 }
